10c58a6a72f117da4857a46804f148dab2d9ada8
fadil-ay <fadilpakoy@gmail.com>
2025-08-31 03:53:08 +0800

    perbaikan

--- resources/views/layout/script.blade.php
+++ resources/views/layout/script.blade.php
@@ -1,20 +1,14 @@
-<!-- Bootstrap core JavaScript-->
-    <script src="{{asset('assets')}}/./vendor/jquery/jquery.min.js"></script>
-    <script src="{{asset('assets')}}/./vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- JS Core -->
+    <script src="{{ asset('assets/vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+    <script src="{{ asset('assets/js/sb-admin-2.min.js') }}"></script>
 
-    <!-- Core plugin JavaScript-->
-    <script src="{{asset('assets')}}/./vendor/jquery-easing/jquery.easing.min.js"></script>
-
-    <!-- Custom scripts for all pages-->
-    <script src="{{asset('assets')}}/./js/sb-admin-2.min.js"></script>
-
-    <!-- Page level plugins -->
-    <script src="{{asset('assets')}}/./vendor/chart.js/Chart.min.js"></script>
-
-    <!-- Page level custom scripts -->
-    <script src="{{asset('assets')}}/./js/demo/chart-area-demo.js"></script>
-    <script src="{{asset('assets')}}/./js/demo/chart-pie-demo.js"></script>
-
+    <!-- Charts -->
+    <script src="{{ asset('assets/vendor/chart.js/Chart.min.js') }}"></script>
+    <script src="{{ asset('assets/js/demo/chart-area-demo.js') }}"></script>
+    <script src="{{ asset('assets/js/demo/chart-pie-demo.js') }}"></script>
+    
    <!-- jQuery (wajib sebelum DataTables) -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
@@ -24,3 +18,6 @@
     <!-- DataTables JS -->
     <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
+
+
+
